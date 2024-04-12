4a309b0b0da2914f1c90ce50ec1092917568589c
Matthew Bunce <mbunce@uoguelph.ca>
2024-04-12 14:27:29 -0400

    burh ast

--- LoxCSharp/GenerateAst.cs
+++ LoxCSharp/GenerateAst.cs
@@ -1,4 +1,15 @@
 namespace CraftingInterpreters.Lox
 {
-    
+    public class GenerateAst {
+        public static void main(String[] args) {
+            if(args.Length != 1){
+                Console.WriteLine("Usage: generate_ast <output directory>");
+                Environment.Exit(64);
+            }
+            String outputDir = args[0];
+
+        }
+    }
+
+
 }
