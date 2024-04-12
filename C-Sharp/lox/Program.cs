5a5622b439a15aa72d87d47f0e3e0991dcbccfa3
Matthew Bunce <mbunce@uoguelph.ca>
2024-04-12 16:44:25 -0400

    setup tool

---
+++ C-Sharp/lox/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace CraftingInterpreters.Lox
+{
+    public class Lox
+    {
+        static bool hadError = false;
+        public static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: jlox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            Run(System.Text.Encoding.UTF8.GetString(bytes));
+            if (hadError) Environment.Exit(65);
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+                hadError = false;
+            }
+        }
+
+        private static void Run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.scanTokens();
+            // For now, just print the tokens.
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        public static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message) {
+            Console.Error.WriteLine("[line " + line + "] Error" + where + ": " + message);
+            hadError = true;
+        }
+    }
+}
--- C-Sharp/tools/GenerateAst.cs
+++ C-Sharp/tools/GenerateAst.cs
@@ -7,10 +7,34 @@ namespace GenerateAst
                 Environment.Exit(64);
             }
             String outputDir = args[0];
-            Console.WriteLine("test" + outputDir);
 
+            DefineAst(outputDir, "Expr", new List<string>
+            {
+                "Binary   : Expr Left, Token Operator, Expr Right",
+                "Grouping : Expr Expression",
+                "Literal  : object Value",
+                "Unary    : Token Operator, Expr Right"
+            });
         }
-    }
 
+        static void DefineAst(string outputDir, string baseName, List<string> types)
+        {
+            string path = Path.Combine(outputDir, baseName + ".cs");
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("using System;");
+                writer.WriteLine("using System.Collections.Generic;");
+                writer.WriteLine();
+                writer.WriteLine($"namespace CraftingInterpreters.Lox");
+                writer.WriteLine("{");
+                writer.WriteLine($"    public abstract class {baseName}");
+                writer.WriteLine("    {");
+
+                writer.WriteLine("    }");
+                writer.WriteLine("}");
+            }
+        }
+
+    }
 
 }
