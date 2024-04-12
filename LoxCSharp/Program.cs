fe0fc82b8e38e87783389f96db34ed20293053d8
Matthew Bunce <mbunce@uoguelph.ca>
2024-04-11 22:08:22 -0400

    4.1

--- LoxCSharp/Program.cs
+++ LoxCSharp/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static CraftingInterpreters.Lox.Scanner;
+
+namespace CraftingInterpreters.Lox
+{
+    public class Lox
+    {
+        static bool hadError = false;
+        public static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: jlox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            Run(System.Text.Encoding.UTF8.GetString(bytes));
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+            }
+        }
+
+        private static void Run(string source)
+        {
+            Scanner scanner = new Scanner(new StreamReader(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(source))));
+            List<Token> tokens = scanner.ScanTokens();
+
+            // For now, just print the tokens.
+            foreach (Token token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message) {
+            Console.Error.WriteLine("[line " + line + "] Error" + where + ": " + message);
+            hadError = true;
+        }
+    }
+}
